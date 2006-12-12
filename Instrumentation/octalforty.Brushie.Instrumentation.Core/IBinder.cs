ec854da0e7732a3ac98a8d07fd1b49ac21145e64
anton.gogolev <anton.gogolev@c4f01733-f12b-0410-952d-d5574990fd28>
2006-12-12 18:15:52 +0000

    * Added Diagnostics assembly (plan to build a performance counter-related framework)

---
+++ Instrumentation/octalforty.Brushie.Instrumentation.Core/IBinder.cs
@@ -0,0 +1,10 @@
+namespace octalforty.Brushie.Instrumentation.Core
+{
+    /// <summary>
+    /// Interface which must be implemented by binders, which route messages
+    /// to various persisters.
+    /// </summary>
+    public interface IBinder
+    {
+    }
+}
---
+++ Instrumentation/octalforty.Brushie.Instrumentation.Diagnostics/PerformanceCounters/PerformanceCounterManager.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace octalforty.Brushie.Instrumentation.Diagnostics.PerformanceCounters
+{
+    class PerformanceCounterManager
+    {
+    }
+}
