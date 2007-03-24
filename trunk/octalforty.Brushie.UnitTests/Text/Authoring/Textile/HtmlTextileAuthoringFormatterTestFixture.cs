5aa41445c6c66f9ae052fff02fd9ad534e3bb8d9
anton.gogolev <anton.gogolev@c4f01733-f12b-0410-952d-d5574990fd28>
2007-03-24 17:50:10 +0000

    Just to illustrate how a simple unit-test helped in finding two bugs :)

---
+++ trunk/octalforty.Brushie.UnitTests/Text/Authoring/Textile/HtmlTextileAuthoringFormatterTestFixture.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+using octalforty.Brushie.Text.Authoring.Textile;
+
+namespace octalforty.Brushie.UnitTests.Text.Authoring.Textile
+{
+    /// <summary>
+    /// <see cref="HtmlTextileAuthoringFormatter"/> unit tests.
+    /// </summary>
+    [TestFixture()]
+    public class HtmlTextileAuthoringFormatterTestFixture
+    {
+        [Test()]
+        public void FormatHeading()
+        {
+            ITextileAuthoringFormatter authoringFormatter =
+                new HtmlTextileAuthoringFormatter();
+
+            Assert.AreEqual("<h1>Heading</h1>",
+                authoringFormatter.FormatHeading(1, "Heading",
+                    new BlockElementAttributes(string.Empty, string.Empty, string.Empty, string.Empty,
+                        BlockElementAlignment.Unknown, 0, 0)));
+        }
+    }
+}
