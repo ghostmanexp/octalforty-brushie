9f0d43882b6b40c0046bc59a1395086007467a3c
anton.gogolev <anton.gogolev@c4f01733-f12b-0410-952d-d5574990fd28>
2007-04-26 14:45:06 +0000

    TableParser.cs * Started writing table parser

--- octalforty.Brushie.Text/Authoring/Textile/TableParser.cs
+++ octalforty.Brushie.Text/Authoring/Textile/TableParser.cs
@@ -2,13 +2,27 @@
 
 namespace octalforty.Brushie.Text.Authoring.Textile
 {
-    public sealed class TableParser
+    /// <summary>
+    /// Provides functionality for parsing Textile tables.
+    /// </summary>
+    public sealed class TableParser : BlockElementParserBase
     {
         #region Private Constants
         private static readonly Regex ExplicitTableRegex = new Regex(
             @"(?<Expression>^(table(\(((\#(?<ID>.+?))|((?<CssClass>.+?)\#(?<ID>.+?))|(?<CssClass>.+?))\))?(\{(?<Style>.+?)\})?(\[(?<Language>.+?)\])?.\r\n))",
                 RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.CultureInvariant |
                 RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+
+        private static readonly Regex TableRegex = new Regex(
+            @"(?<Expression>^((\(((\#(?<ID>.+?))|((?<CssClass>.+?)\#(?<ID>.+?))|(?<CssClass>.+?))\))?(\{(?<Style>.+?)\})?(\[(?<Language>.+?)\].\s)?\|(.+?\|\r\n)+))",
+                RegexOptions.Multiline | RegexOptions.Compiled);
         #endregion
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TableParser"/> class.
+        /// </summary>
+        public TableParser()
+        {
+        }
     }
 }
