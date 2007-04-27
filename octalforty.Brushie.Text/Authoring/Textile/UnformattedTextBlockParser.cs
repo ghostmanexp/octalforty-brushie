98441d774ab35d18da5dd8a98e7748de3a4941c5
anton.gogolev <anton.gogolev@c4f01733-f12b-0410-952d-d5574990fd28>
2007-04-27 10:54:43 +0000

    UnformattedTextBlockParser.cs * UnformattedTextBlockParser now does almost everything it should

--- octalforty.Brushie.Text/Authoring/Textile/UnformattedTextBlockParser.cs
+++ octalforty.Brushie.Text/Authoring/Textile/UnformattedTextBlockParser.cs
@@ -1,4 +1,7 @@
-﻿using octalforty.Brushie.Text.Authoring.Dom;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+using octalforty.Brushie.Text.Authoring.Dom;
 
 namespace octalforty.Brushie.Text.Authoring.Textile
 {
@@ -9,6 +12,11 @@ namespace octalforty.Brushie.Text.Authoring.Textile
     /// </summary>
     public sealed class UnformattedTextBlockParser : InlineElementParserBase
     {
+        #region Private Constants
+        private static readonly Regex QuoteRegex = new Regex(
+            "\\\"(?<Text>(\\S.+?\\S))\\\"", RegexOptions.Compiled);
+        #endregion
+
         /// <summary>
         /// Initializes a new instance of <see cref="UnformattedTextBlockParser"/>.
         /// </summary>
@@ -27,6 +35,44 @@ namespace octalforty.Brushie.Text.Authoring.Textile
         /// <param name="text">The text to be parsed.</param>
         public override void Parse(IAuthoringEngine authoringEngine, DomElement parentElement, string text)
         {
+            //
+            // Unsecaping previously escaped characters.
+            StringBuilder textBuilder = new StringBuilder(text);
+            textBuilder.Replace("\\__", "__");
+            textBuilder.Replace("\\**", "**");
+            textBuilder.Replace("\\_", "_");
+            textBuilder.Replace("\\*", "*");
+            textBuilder.Replace("\\??", "??");
+            textBuilder.Replace("\\?", "?");
+            textBuilder.Replace("\\-", "-");
+            textBuilder.Replace("\\+", "+");
+            textBuilder.Replace("\\^", "^");
+            textBuilder.Replace("\\~", "~");
+            textBuilder.Replace("\\@", "@");
+            textBuilder.Replace("\\%", "%");
+
+            //
+            // Replace double hyphens -- with an em-dash entity.
+            textBuilder.Replace("--", "&mdash;");
+
+            //
+            // Replace single hyphens surrounded by spaces with an en-dash entity.
+            textBuilder.Replace(" - ", "&ndash;");
+
+            //
+            // Replace triplets of periods with an ellipsis entity.
+            textBuilder.Replace("...", "&hellip;");
+
+            //
+            // Convert (TM), (R), and (C) to their respective HTML entities
+            textBuilder.Replace("(TM)", "&trade;");
+            textBuilder.Replace("(R)", "&reg;");
+            textBuilder.Replace("(C)", "&copy;");
+
+            //
+            // Replacing simple quotes with angle quotes
+            text = QuoteRegex.Replace(textBuilder.ToString(), "&laquo;${Text}&raquo;");
+
             parentElement.AppendChild(new TextBlock(parentElement, InlineElementAttributes.Empty, 
                 text, TextBlockFormatting.Unknown));
         }
